a78959afc793094d469fba9642b38f5e53d95a43
AnaelQueant <queant.anael@gmail.com>
2021-03-10 10:07:53 +0100

    Carton réglé

--- Carton/reactcarton/src/Liste_Cartons.js
+++ Carton/reactcarton/src/Liste_Cartons.js
@@ -3,20 +3,28 @@ import TemplateListe from './Template_Liste.js'
 
 class ListeCartons extends React.Component {
 
+
     constructor(props) {
       super(props);
       this.state = {
         value: '',
         text : '',
-        tab : []
+        tab : [],
+        get : props.location.search
       };
     }
     
+    getArgument(elem){
+      var param = elem.split('=');
+      return param[1];
+    }
+
     componentDidMount() {
 
         let monAPI = "http://localhost:16500/lesCartons/" + "1";
 
         console.log("api : " + monAPI);
+        console.log(this.getArgument(this.state.get));
         fetch(monAPI)
             .then(response => response.json())
             .then(response => {
