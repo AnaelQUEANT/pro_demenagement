670fec2ac08677f9b95cd0e6645209c9d3c281a6
Hugo-Fauvel <hugofauvel013@gmail.com>
2021-03-10 09:51:45 +0100

    Ajout fonction

--- Volume/nodeApi/API_Volume.js
+++ Volume/nodeApi/API_Volume.js
@@ -6,3 +6,64 @@ app.use(cors())
 const mysql = require('mysql');
 require('dotenv').config()
 const fetch = require('node-fetch');
+
+
+// Middleware 
+app.use(express.json())
+
+var connection;
+
+function setupConnection(){
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PWD,
+        database: process.env.DB_NAME
+    });
+}
+
+
+
+app.get('/mobilier', (req, res) => {
+    setupConnection();
+    connection.connect((err) => {
+        if (err) throw err;
+        console.log("Connecté !");
+        connection.query('SELECT * from Mobilier', function (error, results, fields) {
+            if (error) throw error;
+            res.send(results);
+            
+        })
+        connection.end();
+    })
+})
+
+
+app.get('/carton', (req, res) => {
+    setupConnection();
+    connection.connect((err) => {
+        if (err) throw err;
+        console.log("Connecté !");
+        connection.query('SELECT * from Carton', function (error, results, fields) {
+            if (error) throw error;
+            res.send(results);
+            
+        })
+        connection.end();
+    })
+})
+
+app.get('/affichette/:idAffichette', (req, res) => {
+    const id = req.params.idAffichette;
+    setupConnection();
+    connection.connect((err) => {
+        if (err) throw err;
+        console.log("Connecté !");
+        connection.query('SELECT * from Piece where Piece_id='+id, function (error, results, fields) {
+            if (error) throw error;
+            res.send(results);
+            
+        })
+        connection.end();
+    })
+})
