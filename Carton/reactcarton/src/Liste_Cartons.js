19b5a158b16dd870cdab21e9a2129509ea8d7fe3
remiragot0010 <44367046+remiragot0010@users.noreply.github.com>
2021-03-15 10:54:22 +0100

    df

--- Carton/reactcarton/src/Liste_Cartons.js
+++ Carton/reactcarton/src/Liste_Cartons.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import TemplateListe from './Template_Liste.js'
-
+import './ListeCartons.css';
 
 class ListeCartons extends React.Component {
 
@@ -32,12 +32,20 @@ class ListeCartons extends React.Component {
                 var tableauID = [];
             
                 for(var i=0;i<response.length;i++){
-                    test[i] = { photo:  response[i].Carton_photo, id : response[i].Carton_id, origine :  response[i].Carton_origine, destination : response[i].Piece_nom, dim : response[i].Carton_largeur + "x" +  response[i].Carton_longueur + "x" +  response[i].Carton_hauteur, fragile : response[i].Carton_fragile,};
+                  if(response[i].Carton_fragile == 1){
+                    test[i] = { photo:  response[i].Carton_photo, id : response[i].Carton_id, origine :  response[i].Carton_origine, destination : response[i].Piece_nom, dim : response[i].Carton_largeur + "x" +  response[i].Carton_longueur + "x" +  response[i].Carton_hauteur, fragile : "ImageFragile"};
+
+                  }else{
+                    test[i] = { photo:  response[i].Carton_photo, id : response[i].Carton_id, origine :  response[i].Carton_origine, destination : response[i].Piece_nom, dim : response[i].Carton_largeur + "x" +  response[i].Carton_longueur + "x" +  response[i].Carton_hauteur, fragile : ""};
+
+                  }
                     
                 }
-                console.log(this.state.tab.length);
+                console.log(this.state.tab.length + " YOPPPP");
+      
                 var listItems = this.state.tab.map(e => (
-                    <TemplateListe photo={e.photo} id={e.id} origine={e.origine} destination={e.destination} dim={e.dim} fragile={e.fragile} idBouton={e.id} />
+
+                    <TemplateListe  photo={e.photo} id={e.id} origine={e.origine} destination={e.destination} dim={e.dim} fragile={e.fragile} idBouton={e.id} />
         
                 ));
                 this.setState({text:listItems})
@@ -47,7 +55,7 @@ class ListeCartons extends React.Component {
 
     render() {
       return (
-        <div class="row">
+        <div class="row test">
             {this.state.text}
         </div>
       )
