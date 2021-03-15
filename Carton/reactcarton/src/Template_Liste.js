2f969f6573b8566ef276262767bcee1f692b65ae
remiragot0010 <44367046+remiragot0010@users.noreply.github.com>
2021-03-15 08:28:09 +0100

    Le push du matin

--- Carton/reactcarton/src/Template_Liste.js
+++ Carton/reactcarton/src/Template_Liste.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import boutonModif from './Image/bouton_modifier.PNG';
+import ImageCarton from './Image/carton.jpg';
+import {Link} from 'react-router-dom'
+
 
 class TemplateListe extends React.Component {
     render() {
@@ -7,21 +11,25 @@ class TemplateListe extends React.Component {
             
         
                 <div className="row">
-                    <div className="col-3">
-                       {this.props.photo}
+                    <div className="col-2">
+                       {/*{this.props.photo}*/}
+                       <img className="ImageCarton" src={ImageCarton} width="5%" height="5%" alt="image temporaire de carton"></img>
                     </div>
-                    <div className="col-3">
+                    <div className="col-2">
                         {this.props.id}
                     </div>
-                    <div className="col-3">
+                    <div className="col-5">
                         <span>{this.props.origine} vers {this.props.destination}</span>
                         <br/>
                         <span>Dim : {this.props.dim}</span>
                         <br/>
                         <span>{this.props.fragile}</span>
                     </div>
-                    <div className="col-3" >
-                        <a className="nav-link" aria-current="page" href={"ModifierCarton?id="+this.props.id}>Modifier</a>
+                    <div className="col-2" >
+                        <Link to={"ModifierCarton?id="+this.props.id}>
+                            <img className="boutonModif" src={boutonModif} width="5%" height="5%" alt="Lien modif de carton"></img>
+                
+                        </Link>
                     </div>
                     
                 </div>
