5b978f8cde5ce5bc53a3d09fd83b6209455d407c
AnaelQueant <queant.anael@gmail.com>
2021-03-16 11:02:17 +0100

    Recup

--- Carton/reactcarton/src/Menu_Carton.js
+++ Carton/reactcarton/src/Menu_Carton.js
@@ -1,35 +1,37 @@
 import React from 'react'
 import './MenuCarton.css';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import ImageChevron from './Image/chevron2.png';
 import ImageInfo from './Image/info.png';
 
 
 class MenuCarton extends React.Component {
 
-    constructor(props) {
-      super(props);
-    }
-
-    render() {
-      return (
-        <div className="row col-12 liste-elem">
-                    
-                    <div className="col-2 photo-info">
-                       <img className="ImageCarton" src={ImageInfo} width="160%"  alt="image temporaire de carton"></img>
-                    </div>
-                    <div className="col-6 liste-info" >
-                        <span>Préparer</span>
-                        <br/>
-                        <span>ses cartons</span>
-
-                    </div>
-                    <Link to="ListeSalles">
-                        <img className="ImageChevron" src={ImageChevron}  alt="image temporaire de carton"></img>
-                    </Link>
-                    
-        </div>
-      )
-    }
+  constructor(props) {
+    super(props);
   }
-  export default MenuCarton
+
+  render() {
+    return (
+      <Link to="ListeSalles" >
+        <div className="row liste-elem">
+
+          <div className="col-2 photo-info">
+            <img className="ImageCarton" src={ImageInfo} width="160%" alt="image temporaire de carton"></img>
+          </div>
+          <div className="col-6 liste-info" >
+            <span>Préparer</span>
+            <br />
+            <span>ses cartons</span>
+
+          </div>
+
+          <img className="ImageChevron col-4" src={ImageChevron} alt="image temporaire de carton"></img>
+
+
+        </div>
+      </Link >
+    )
+  }
+}
+export default MenuCarton
