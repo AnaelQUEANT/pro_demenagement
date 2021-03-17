bd98fd464795ad2f90bf83cd26401520e457f4dd
remiragot0010 <44367046+remiragot0010@users.noreply.github.com>
2021-03-17 11:07:50 +0100

    TEster c'est douter, mais je pense que ça marche pas

--- Carton/reactcarton/src/Template_FormulaireModification.js
+++ Carton/reactcarton/src/Template_FormulaireModification.js
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import './CSS/CreationCarton.css';
+import cartonAjout from './Image/AjoutImage.PNG';
 class Template_FormulaireModification extends React.Component {
   constructor(props) {
     super(props);
@@ -19,75 +20,206 @@ class Template_FormulaireModification extends React.Component {
       largeurError: '',
       longueurError: '',
       origineError: '',
-      inputFragile: '',
+      destinationError: '',
+      inputFragile: this.props.fragile,
       inputDestination: this.props.destination,
-      selectCouleur: '',
+      selectCouleur: this.props.couleur,
       inputFile: '',
       idDestination: '',
-      valID: ''
+      valID: '',
+      get: props.location.search
     };
+  }
+
+  
+  getArgument(elem) {
+    var param = elem.split('=');
+    return param[1];
   }
 
   handleChange = event => {
     console.log(event.target.value + " et " + [event.target.id]);
     this.setState({ [event.target.id]: event.target.value });
   };
+  validate = () => {
+    let hauteurError = "";
+    let origineError = "";
+    let largeurError = "";
+    let longueurError = "";
+    let destinationError = "";
+    if (!this.state.inputLargeur) {
+      largeurError = "Le largeur est vide";
+    }
+    if (!this.state.inputLongueur) {
+      longueurError = "Le longueur est vide";
+    }
+    if (!this.state.inputHauteur) {
+      hauteurError = "La hauteur est vide";
+    }
+    if (!this.state.origine) {
+      origineError = "L'origine est vide";
+    }
+    if (!this.state.inputDestination) {
+      origineError = "La destination est vide";
+    }
+    this.setState({ largeurError });
+    this.setState({ longueurError });
+    this.setState({ hauteurError });
+    this.setState({ origineError });
+    this.setState({ destinationError });
+    if (!this.state.inputLargeur || !this.state.inputLongueur || !this.state.inputHauteur || !this.state.origine || !this.state.inputDestination) {
+      return false;
+    }
+    return true;
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+    const isValid = this.validate();
+    if (isValid) {
+      console.log(this.state);
+      var elementCheck = document.getElementById('inputFragile').checked;
+      var nomDestination = document.getElementById('inputDestination').value;
+      var row = this.state.tab2.map(function (cell) {
+        console.log("ICIIIIIIIII " + cell.nom + " et " + nomDestination + " donc " + cell.id);
+        if (cell.nom == nomDestination) {
+          this.state.idDestination = cell.id;
+
+          return cell.id;
+        }
+      }.bind(this));
+      try {
+        let monAPI = "http://localhost:16500/UpdateCarton/" + this.getArgument(this.state.get);
+        if (elementCheck) {
+          elementCheck = 1;
+        } else {
+          elementCheck = 0;
+        }
+        console.log(this.state.origine + this.state.selectCouleur + this.state.inputLargeur + this.state.inputLongueur + this.state.inputHauteur + elementCheck + this.state.idDestination);
+
+        fetch(monAPI, {
+          method: 'PUT',
+          headers: { 'content-type': 'application/json' },
+          body: JSON.stringify({
+            "origine": this.state.origine,
+            "couleur": this.state.selectCouleur,
+            "largeur": this.state.inputLargeur,
+            "longueur": this.state.inputLongueur,
+            "hauteur": this.state.inputHauteur,
+            "fragile": elementCheck,
+            "piece": this.state.idDestination
+          })
+
+        })
+      } catch (e) {
+        console.log(e);
+      }
 
 
+      /*var row = this.state.tab.map(function(cell) {
+        var value = "id" + cell.id;
+        var elemID = document.getElementByID(value).checked;
+        if(elemID){
+          try{
+            let monAPI = "http://localhost:16500/ajoutObjetCarton";
+            fetch(monAPI, {
+              method: 'POST',
+              headers: { 'content-type': 'application/json' },
+              body: JSON.stringify({
+              })
+            })
+          }catch(e){
+            console.log(e);
+          }
+            this.state.idDestination = cell.id;
+            return cell.id;       
+        }
+      }.bind(this));
+      //this.setState(initialState);
+      */
+      this.props.history.push('/ListeSalles');
+    }
+  };
   render() {
     return (
-      <form>
-        <br />
-        <div>
-          <form>
-            <div class="form-group">
-              {this.props.photo}
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <br />
+          <div className="row">
+            <img className="col-6" src={cartonAjout} alt="Lien select carton (Image absente)"></img>
+            <div className="col-6">
+              <div className="col-">
+                <label>Couleur</label>
+                <br />
+                <select id="selectCouleur" width="100%" value={this.state.selectCouleur} onChange={this.handleChange}>
+                  <option></option>
+                  <option>Rouge</option>
+                  <option>Bleu</option>
+                  <option>Vert</option>
+                  <option>Jaune</option>
+                  <option>Violet</option>
+                </select>
+              </div>
+              <br />
+              <br />
+              <div>
+                <input type="checkbox" id="inputFragile" value={this.state.fragile} name="Fragile" onChange={this.handleChange} />
+                <label for="Fragile">Fragile</label>
+              </div>
             </div>
-          </form>
-        </div>
-        <br />
-        <select id="selectCouleur" width="100%" value={this.props.couleur} onChange={this.handleChange}>
-          <option></option>
-          <option>Rouge</option>
-          <option>Bleu</option>
-          <option>Vert</option>
-          <option>Jaune</option>
-          <option>Violet</option>
-        </select>
-        <br />
-        <div>
-          <div>
-            <input type="checkbox" id="Fragile" name="Fragile" value={this.state.fragile} />
-            <label for="Fragile">Fragile</label>
           </div>
-        </div>
-        <br />
-        <div class="form-group">
-          <input type="text" class="form-control" id="destination" value={this.state.inputDestination} placeholder="Destination" onChange={this.handleChange}></input>
-        </div>
-        <br />
-        <div class="form-group">
-          <input type="text" class="form-control" id="origine" value={this.state.origine} onChange={this.handleChange} placeholder="Origine" />
-        </div>
-        <br />
-        <div>
-          <div>
-            <label>Largeur</label>
-            <input type="number" class="form-control" id="inputLargeur" value={this.state.inputLargeur} onChange={this.handleChange} placeholder="Largueur"></input>
+          <br />
+          <div class="form-group">
+            <div class=" ">
+              <input class="input100 form-control" type="text" name="name" placeholder="Destination" id="inputDestination" value={this.state.inputDestination} onChange={this.handleChange} />
+              <div style={{ fontSize: 12, color: "red" }}>
+                {this.state.destinationError}
+              </div>
+            </div>
+          </div>
+          <br />
+          <div class="form-group">
+            <div class=" ">
+              <input class="input100 form-control" type="text" name="name" placeholder="Origine" id="origine" value={this.state.origine} onChange={this.handleChange} />
+              <div style={{ fontSize: 12, color: "red" }}>
+                {this.state.origineError}
+              </div>
+            </div>
+          </div>
+          <br />
+          <div >
+            <div className="row">
+              <div className="col-4 ">
+                <input type="number" className="form-control input101" id="inputLargeur" value={this.state.inputLargeur} placeholder="Largeur" onChange={this.handleChange} ></input>
+                <div style={{ fontSize: 12, color: "red" }}>
+                  {this.state.largeurError}
+                </div>
+              </div>
+
+              <div className="col-4"> 
+                <input type="number" className="form-control input101" id="inputLongueur" placeholder="Longueur" value={this.state.inputLongueur} onChange={this.handleChange}></input>
+                <div style={{ fontSize: 12, color: "red" }}>
+                  {this.state.longueurError}
+                </div>
+              </div>
+
+              <div className="col-4">
+                <input type="number" className="form-control input101" id="inputHauteur" placeholder="Hauteur" value={this.state.inputHauteur} onChange={this.handleChange}></input>
+                <div style={{ fontSize: 12, color: "red" }}>
+                  {this.state.hauteurError}
+                </div>
+
+              </div>
+
+            </div>
           </div>
           <br />
           <div>
-            <label>Longueur</label>
-            <input type="text" class="form-control" id="inputLongueur" value={this.state.inputLongueur} onChange={this.handleChange} placeholder="Longueur"></input>
+            {this.state.text}
           </div>
           <br />
-          <div>
-            <label>Hauteur</label>
-            <input type="text" class="form-control" id="inputHauteur" value={this.state.inputHauteur} onChange={this.handleChange} placeholder="Hauteur"></input>
-          </div>
-        </div>
-        <br />
-      </form>
+          <input type="submit" value="Créer" />
+        </form>
+      </div>
     );
   }
 }
