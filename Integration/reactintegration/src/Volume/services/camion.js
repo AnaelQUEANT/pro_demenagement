b01f12f1c1105884aa4a3e8fc137dd1e39e73447
jerôme Trébern <jerome.trebern@gmail.com>
2021-03-19 10:52:36 +0100

    resolution bug

--- Integration/reactintegration/src/Volume/services/camion.js
+++ Integration/reactintegration/src/Volume/services/camion.js
@@ -1,6 +1,7 @@
 export const getCamion = async (vol) => {
+    console.log('vol: '+vol)
     const url = 'http://localhost:16500/camion/'+vol
     const request = await fetch(url)
-    const result = await request.json()
+    const result = await request.json()    
     return result
   }
